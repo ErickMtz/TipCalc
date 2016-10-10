0be22d46254fe5a3e9a56f001913a63a03b47012
Erick Martínez <erick.e_274@hotmail.com>
2016-10-09 20:51:19 -0500

    Add getTipPercentage

--- app/src/main/java/com/erickmtz/tipcalc/MainActivity.java
+++ app/src/main/java/com/erickmtz/tipcalc/MainActivity.java
@@ -1,42 +1,143 @@
 package com.erickmtz.tipcalc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_CHANGE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.action_about){
+        if (item.getItemId() == R.id.action_about) {
             about();
         }
         return super.onOptionsItemSelected(item);
     }
 
+    @OnClick(R.id.btnSubmit)
+    public void handleSubmit() {
+        hideKeyboard();
+
+        String strInputTotal = inputBill.getText().toString().trim();
+
+        if(!strInputTotal.isEmpty()) { //con ! negamos el resultado
+            double total = Double.parseDouble(strInputTotal);
+            int tipPercentage = getTipPercentage();
+
+            double tip = total * (tipPercentage/100d);
+
+            String strTip = String.format(getString(R.string.global_message_tip),tip);
+            txtTip.setVisibility(View.VISIBLE);
+            txtTip.setText(strTip);
+
+        }
+    }
+
+    @OnClick(R.id.btnIncrease)
+    public void handleClickIncrease() {
+        // Cuando des click a + debe llamar a handleTipChange y sumar 1
+        handleTipChange(TIP_STEP_CHANGE);
+    }
+
+    @OnClick(R.id.btnDecrease)
+    public void handleClickDecrease() {
+        handleTipChange((-1)*TIP_STEP_CHANGE);
+        // Cuando des click a - debe llamar a handleTipChange y restar 1
+    }
+
+    public int getTipPercentage() {
+        //1 crear una variable tipPercentage en la que guardemos DEFAULT_TIP_CHANGE
+        int tipPercentage = DEFAULT_TIP_CHANGE;
+        //2 crear una variable String strInputTipPercentage que tome el valor del inputPercentage (no olvidar el trim)
+        String strInputTipPercentage = inputPercentage.getText().toString().trim();
+        // 3 Verificar que la cadena no venga vacia
+        if(!strInputTipPercentage.isEmpty()) {
+            tipPercentage = Integer.parseInt(strInputTipPercentage);
+        }
+        else{
+            inputPercentage.setText(String.valueOf(DEFAULT_TIP_CHANGE));
+            //inputPercentage.setText(String.value0f(DEFAULT_TIP_PERCENTAGE));
+        }
+        // 3a Si no viene vacia sobreescribir tipPercentage con el valor de StrTipPercentage (no olvien convertirlo a entero)
+        // 3b inputPercentage.setText(String.value0f(DEFAULT_TIP_PERCENTAGE));
+        // 4 Devolver el valor de tipPercentage
+        return tipPercentage;
+    }
+
+    public void handleTipChange(int change) {
+        // 1 Llamar a getTipPercentage (en una variable)
+        int percentage = getTipPercentage();
+        // 2 Aplicar el incremento/decremento que vienen en la variable change
+        percentage += change;
+        // 3 si tipPercentage mayor que 0 entonces colocar el valor en el valor del incremento en el input
+        if(percentage>0){
+            inputPercentage.setText(String.valueOf(percentage));
+        }
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        try{
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException npe){
+            Log.e(getLocalClassName(),Log.getStackTraceString(npe));
+        }
+    }
+
     private void about() {
-        TipCalcApp app = (TipCalcApp)getApplication();
+        TipCalcApp app = (TipCalcApp) getApplication();
         String strUrl = app.getAboutUrl();
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse(strUrl));
         startActivity(intent);
     }
+
+
 }
